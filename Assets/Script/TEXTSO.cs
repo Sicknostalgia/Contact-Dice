a0b873a156d1799d1381e9a87b195437bd6cc32f
Sicknostalgia <89644728+Sicknostalgia@users.noreply.github.com>
2025-02-28 13:02:00 +0800

    introduce Text container

---
+++ Assets/Script/TEXTSO.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "TextContainer/")]
+public class TEXTSO : ScriptableObject
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
