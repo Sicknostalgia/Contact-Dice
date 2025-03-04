89ebd680dea49d04695f209fc8b59bfd2d08badb
Sicknostalgia <89644728+Sicknostalgia@users.noreply.github.com>
2025-03-05 00:31:31 +0800

    creating rewind mechanics

--- Assets/RewindTime.cs
+++ Assets/RewindTime.cs
@@ -1,19 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public class ManageScene : MonoBehaviour
+public class RewindTime : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
+        
+    }
 
-    }
-    public void RestartScene()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-    }
     // Update is called once per frame
     void Update()
     {
