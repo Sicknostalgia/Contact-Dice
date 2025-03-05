a41a689cabc975bd50bb4fd38b7721dc6ba94674
Sicknostalgia <89644728+Sicknostalgia@users.noreply.github.com>
2025-03-05 12:40:22 +0800

    rewinding!!!

--- Assets/RewindTime.cs
+++ Assets/RewindTime.cs
@@ -1,27 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class RewindTime : MonoBehaviour
 {
-    List<Vector3> positions;
-    bool isRewinding = false;
+     List<Vector3> positions;
+    public bool isRewinding = false;
+    [SerializeField] 
+    Rigidbody rb;
     void Start()
     {
         positions = new List<Vector3>();
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (isRewinding) Rewind();
+        else Record();
+    }
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Rewind();
+        }
+    }
+    void Rewind()
+    {
+        if (positions.Count > 0)
+        {
+            transform.position = positions[0];
+            positions.RemoveAt(0);  //remove right after...
+        }
+        else StopRewind();
+    }
+    void Record()
+    {
+        positions.Insert(0, transform.position);
     }
     public void StartRewind()
     {
         isRewinding = true;
+        rb.isKinematic = true;
     }
     public void StopRewind()
     {
         isRewinding = false;
+        rb.isKinematic = false;
     }
 }
