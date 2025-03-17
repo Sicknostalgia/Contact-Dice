81285e471ed69fe575904ed122add41d5cdb9261
Sicknostalgia <89644728+Sicknostalgia@users.noreply.github.com>
2025-03-17 14:18:01 +0800

    Added UnityEvent variable to access in Inpector
    remove MathF.Pingpong

--- Assets/HoverOverPlayer.cs
+++ Assets/HoverOverPlayer.cs
@@ -1,34 +1,30 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.Events;
 
 public class HoverOverPlayer : MonoBehaviour,IPointerClickHandler,IPointerEnterHandler,IPointerExitHandler
 {
     public Material selectionOutlineMat;
+    bool isPointerInside = false;
+    [SerializeField] float value;
+    public UnityEvent onPointerEnter;
+    public UnityEvent onPointerExit;
+    public UnityEvent onPointerClick;
     public void OnPointerEnter(PointerEventData eventData)
     {
-        float value = Mathf.PingPong(Time.time, 1.1f);
+        onPointerEnter?.Invoke();
+        isPointerInside = true;
         selectionOutlineMat.SetFloat("_scale", value); // Modify shader variable
     }
     public void OnPointerExit(PointerEventData eventData)
     {
-        float value = Mathf.PingPong(Time.time, 0f);
-        selectionOutlineMat.SetFloat("_scale", value); // Modify shader variable
+        onPointerExit?.Invoke();
+        isPointerInside = false;
+        selectionOutlineMat.SetFloat("_scale", 0); // Modify shader variable
     }
 
     public void OnPointerClick(PointerEventData eventData)
     {
-
-    }
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        onPointerClick?.Invoke();
     }
 }
