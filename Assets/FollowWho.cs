6027cbbfbbc698565f0e929902ecf79e40bada47
Sicknostalgia <89644728+Sicknostalgia@users.noreply.github.com>
2025-03-17 11:54:08 +0800

    smoothed Camera(fixed to lateupdate)

--- Assets/FollowWho.cs
+++ Assets/FollowWho.cs
@@ -11,7 +11,7 @@ public class FollowWho : MonoBehaviour
     {
         if (!ManageScene.isPlaying)
         {
-            Assigned();
+            DeAssigned();
         }
         /*if (!ManageScene.isPlaying)
         {
